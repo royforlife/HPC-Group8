e1c2670c9f2a7e40e0b87834ecc78ab46411b50b
Yihong Xi <y.xi2@student.vu.nl>
2023-01-20 13:24:04 +0100

    new graph

--- asg2/3/output.py
+++ asg2/3/output.py
@@ -1,14 +1,19 @@
 import os
 filepath = "./"
 path_list = os.listdir(filepath)
+res = []
 for path in path_list:
     if '.out' in path:
         file = open("./"+path, mode='r')
         lines = file.readlines()
-        print(path)
-        res = []
+        # print(path)
+        # res = []
         for line in lines:
+            if 'CPUS' in line:
+                print(line[5:-1])
             if 'obtained' in line:
                 data = line.split(' ')
+                print(float(data[-2]))
                 res.append(float(data[-2]))
-        print(res)
+        print(' ')
+print(res)
